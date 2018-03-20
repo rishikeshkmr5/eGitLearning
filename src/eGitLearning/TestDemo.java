9bf3168a88396f606cf3943ce7720e6470eee416
rishikeshkmr5 <rishikeshkmr5@gmail.com>
2018-03-20 11:13:02 +0000

    Update TestDemo.java

--- src/eGitLearning/TestDemo.java
+++ src/eGitLearning/TestDemo.java
@@ -4,6 +4,9 @@ public class TestDemo {
 public static void main(String[] args) {
 
 	System.out.println("Hi this is merged msg after resolution");
+	System.out.println("learning staashing step1");
+	
+	
 
 }
 }
